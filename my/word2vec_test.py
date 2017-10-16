bdd48e7812c40124ce6acd654f60ae7bbeafe5db
liuyan113063 <liuyan113063@pwrd.com>
2017-10-16 14:20:41 +0800

    update

--- my/word2vec_test.py
+++ my/word2vec_test.py
@@ -15,6 +15,8 @@ import zipfile
 import numpy as np
 import urllib.request
 import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
 
 
 # 下载文本数据，并核对文件尺寸
@@ -38,9 +40,11 @@ def read_data(filename):
     return data
 
 
+vocabulary_size = 50000
+
+
 # 按照单词出现的频数创建词汇表，词频top50000的词将放入到词汇表中，top50000以外的词单独编号为0
 def bulid_dataset(words):
-    vocabulary_size = 50000
     count = [['UNK', -1]]
     count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
     dictionary = dict()
@@ -90,6 +94,16 @@ def generate_batch(batch_size, num_skips, skip_window, data):
     return batch, labels
 
 
+def plot_with_labels(low_dim_embs, labels, filename="tsen.png"):
+    assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
+    plt.figure(figsize=(18, 18))
+    for i, label in enumerate(labels):
+        x, y = low_dim_embs[i, :]
+        plt.scatter(x, y)
+        plt.annotate(label, xy=(x, y), xytext=(5, 2), textcoords='offset points', ha='right', va='bottom')
+    plt.savefig(filename)
+
+
 if __name__ == '__main__':
     filename = maybe_download('text8.zip', 31344016)
     words = read_data(filename)
@@ -109,10 +123,61 @@ if __name__ == '__main__':
     valid_examples = np.random.choice(valid_window, valid_size, replace=False)  # 随机从频数最高的100个单词中抽取16个作为验证单词
     num_sampled = 64  # 训练时作为负样本的噪声单词数量
 
-    batch, labels = generate_batch(batch_size=batch_size, num_skips=num_skips, skip_window=skip_window, data=data)
+    # batch, labels = generate_batch(batch_size=batch_size, num_skips=num_skips, skip_window=skip_window, data=data)
 
+    # 定义Skip0Gram Word2Vec模型的网络结构
     graph = tf.Graph()
     with graph.as_default():
         train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
         train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
         vallid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+        with tf.device('/cpu:0'):
+            embedding = tf.Variable(tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+            embed = tf.nn.embedding_lookup(embedding, train_inputs)
+            nce_weights = tf.Variable(tf.truncated_normal([vocabulary_size, embedding_size],
+                                                          stddev=1.0 / math.sqrt(embedding_size)))
+            nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+        loss = tf.reduce_mean(tf.nn.nce_loss(weights=nce_weights, biases=nce_biases, labels=train_labels, inputs=embed,
+                                             num_sampled=num_sampled, num_classes=vocabulary_size))
+        optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+        norm = tf.sqrt(tf.reduce_sum(tf.squeeze(embedding), 1, keep_dims=True))
+        normalized_embeddings = embedding / norm
+        valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, vallid_dataset)
+        similarity = tf.matmul(valid_embeddings, normalized_embeddings, transpose_b=True)
+        init = tf.global_variables_initializer()
+
+    # 训练
+    num_steps = 100001
+    with tf.Session(graph=graph) as session:
+        init.run()
+        print("Initialized")
+
+        average_loss = 0
+        for step in range(num_steps):
+            batch_input, batch_labels = generate_batch(batch_size, num_skips, skip_window, data=data)
+            feed_dict = {train_inputs: batch_input, train_labels: batch_labels}
+            _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
+            average_loss += loss_val
+            if step % 2000 == 0:
+                if step > 0:
+                    average_loss /= 2000
+                print("Average loss at step ", step, ": ", average_loss)
+                average_loss = 0
+            if step % 10000 == 0:
+                sim = similarity.eval()
+                for i in range(valid_size):
+                    valid_word = reverse_dictionary[valid_examples[i]]
+                    top_k = 8
+                    nearest = (-sim[i, :]).argsort()[1:top_k+1]
+                    log_str = "Nearest to %s" % valid_word
+                for k in range(top_k):
+                    close_word = reverse_dictionary[nearest[k]]
+                    log_str = "%s %s, " % (log_str, close_word)
+                print(log_str)
+        final_embeddings = normalized_embeddings.eval()
+
+    tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+    plot_only = 100
+    low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
+    labels = [reverse_dictionary[i] for i in range(plot_only)]
+    plot_with_labels(low_dim_embs, labels)
